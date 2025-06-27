3e033b8030f8fa0565bcbb8f0f6266615715fde4
Salim Shrestha <salim9shrestha@gmail.com>
2025-06-27 23:42:28 +0545

    done.

--- Third Semester/Data Structure And Algorithm/DSA LAB 01/q5.cpp
+++ Third Semester/Data Structure And Algorithm/DSA LAB 01/q5.cpp
@@ -1,3 +1,26 @@
 //write an algorithm and program to add numbers using pointers in c/c++
 #include <stdio.h>
 
+int main(){
+    int a, b, sum;
+    int *p1, *p2, *p3;
+
+    // Assigning addresses to pointers
+    p1 = &a;
+    p2 = &b;
+    p3 = &sum;
+
+    // Input values
+    printf("Enter first number: ");
+    scanf("%d", p1);
+    printf("Enter second number: ");
+    scanf("%d", p2);
+
+    // Calculate sum using pointers
+    *p3 = *p1 + *p2;
+
+    // Output result
+    printf("Sum of %d and %d is %d\n", *p1, *p2, *p3);
+
+    return 0;
+}
