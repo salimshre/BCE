b59687a902e2301eff706d3bc2307182c2303ade
Salim Shrestha <salim9shrestha@gmail.com>
2025-07-01 19:17:05 +0545

    done.

--- Second Semester/fun-over.cpp
+++ Second Semester/fun-over.cpp
@@ -1,17 +1,19 @@
 #include<iostream>
-
 using namespace std;
-
 
 int over (int a)
 {
+    cout<<"the value of a is"<< a <<endl;
+}
 
+int over (int a, float b){
+    cout<<"the value of a and b is"<<a << b<<endl;
 }
 
 
-
-int min()
+int main()
 {
-    int over(int a);
-    int over(int a, float b);
+    over(1);
+    over(2, 2.5);
+ 
 }
