63ff53e83f667e773c36a2e0a86a2410c12cec0c
Salim Shrestha <salim9shrestha@gmail.com>
2025-08-07 23:49:56 +0545

    update

---
+++ Fourth Semester/python/ut.py
@@ -0,0 +1,9 @@
+marks = int(input("enter marks?"))
+if marks<= 10:
+    print("the rade is 10%")
+elif marks<= 20:
+        print("the rade is 5%")
+elif marks<= 30:
+        print("the rade is 2%")
+else:
+        print("nothing")
